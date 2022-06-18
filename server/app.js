1f959ca918e11ddbe0b6ad081d17d793e9e389e9
Ofer Tauber <ofertauber@gmail.com>
2022-06-18 11:39:31 +0300

    Hope to deploygit add .!

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const route = require('./src/route');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,6 +11,12 @@ app.use(cors());
 app.use(express.text());
 app.use(express.json());
 app.use('/', route);
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('/*', (req, res) => {
+  void req;
+  res.sendFile(path.join(__dirname + '../client/build/index.html'));
+});
 
 app.listen(PORT, (req, res) => {
   void req, res;
