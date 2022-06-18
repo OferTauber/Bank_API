e678c264e41c9003a5b043c1c58ad06cc189f3d5
Ofer Tauber <ofertauber@gmail.com>
2022-06-18 13:15:57 +0300

    a

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const URL = process.env.PORT ?? 'http://localhost:5000';
+const URL =
+  process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:5000';
 
 const sendReqwest = async (type, route, params) => {
   try {
